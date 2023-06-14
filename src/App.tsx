78447c329d5edbf3264b758e13bb2fd7b2869e0c
GregisGinger <Greg12thomson@Gmail.com>
2023-06-14 16:28:26 +0200

    Move to typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>HELLO THERE 👋, I'M</h3>
+        <h1>
+          Greg
+        </h1>
+        <p>FULL-STACK DEVELOPER</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
