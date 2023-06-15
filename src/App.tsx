cf8b47870aa5129b6f02d8f58545ee97b8c8384e
GregisGinger <Greg12thomson@Gmail.com>
2023-06-15 14:40:20 +0200

    Add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Container, Col, Row } from 'react-bootstrap';
+
 
 import './App.css';
 
@@ -6,12 +8,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>HELLO THERE 👋, I'M</h3>
-        <h1>
-          Greg
-        </h1>
-        <p>FULL-STACK DEVELOPER</p>
+        <Container>
+          <Row className="hero">
+            <Col md={8}>
+              <h3>Hello There 👋, I'm</h3>
+              {/* <h2>Hello, my name is <strong>Greg</strong></h2> */}
+              <h1>
+                Greg  
+              </h1>
+              {/* <h4>I'm a <strong>Full-Stack Developer</strong> from Edinburgh, Scotland.</h4> */}
+              <h4>Full-Stack Developer</h4>
+              <p>Amazon Web Services (AWS) • Java • JavaScript • TypeScript • CDK • SQL • Node • Python • Linux • Cordova</p>
+            </Col>
+          </Row>
+        </Container>
       </header>
+      <div className="experience">
+          <Container>
+            <Row>
+              <Col md={12}>
+                <h1>Past experience</h1>
+              </Col>
+            </Row>
+            <hr/>
+          </Container>
+        </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router-dom";
+import './styles/header.scss'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
