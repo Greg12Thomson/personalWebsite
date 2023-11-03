95d0fb75d224b95555601b3326fd57bbb5078fd8
GregisGinger <Greg12thomson@Gmail.com>
2023-11-03 14:14:25 +0000

    Reformat

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Col, Container, Row} from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function Experience() {
     return (
@@ -7,10 +7,10 @@ function Experience() {
             <Container className="experience">
                 <Row>
                     <Col md={12}>
-                    <h1>Experience</h1>
+                        <h1>Experience</h1>
                     </Col>
                 </Row>
-                <hr/>
+                <hr />
 
                 <h3 className="summary-heading">Experienece Summary</h3>
                 <ul className="summary-list">
@@ -26,8 +26,8 @@ function Experience() {
                     <li>1 year working for Prime Video Livingroom implementing a hybrid mechanism to allow for
                         the new Prime Video application to be created</li>
                 </ul>
-    
-        
+
+
 
                 {/* AWS */}
                 <Row className="experience-card">
@@ -35,38 +35,38 @@ function Experience() {
                         <p>Oct 2020 - Dec 2022</p>
                     </Col>
                     <Col md={10}>
-                    <h3>Software Engineer II <span className="purple">•</span> AWS</h3>
-                    <h5 className="subheading">DevOps Guru & CodeGuru Profiler - London, UK</h5>
-                    <p>
-                        Working as a full-stack engineer across CodeGuru Profiler and DevOps Guru.
-                        I worked with Senior Product Managers, Principal Engineers, Senior UX Designers,
-                        Documentation writers and Managers to launch new features for both {" "}
-                        <a href="https://aws.amazon.com/codeguru/">CodeGuru Profiler</a> and {" "} 
-                        <a href="https://aws.amazon.com/devops-guru/">DevOps Guru</a>. I led projects such as DevOps
-                        Guru integration into RDS which led to a 30% increase in customers. I also designed,
-                        implemented, and launched new customer-facing features for both products, such as profile
-                        comparison visualizations and a new onboarding workflow.<br/><br/>
-                        
-                        I also regularly improved and simplified development and operational processes such as: <br/>
-                        - Improving our teams pipelines to be fully CD and ran a project to move all of the product's
-                        pipelines to full CD. <br/>
-                        - Moved away from manually creating alarms and moved all our remaining infrastructure to CDK. <br/>
-                        - Fixed over-provisioned databases which led to a ~$1 million saving annually.
-                    </p>
-                    <ul className="skills">
-                        <li>Java</li>
-                        <li>Typescript</li>
-                        <li>CDK</li>
-                        <li>JavaScript</li>
-                        <li>AWS</li>
-                        <li>React</li>
-                        <li>DynamoDB</li>
-                        <li>Lambda</li>
-                        <li>RDS</li>
-                    </ul>
+                        <h3>Software Engineer II <span className="purple">•</span> AWS</h3>
+                        <h5 className="subheading">DevOps Guru & CodeGuru Profiler - London, UK</h5>
+                        <p>
+                            Working as a full-stack engineer across CodeGuru Profiler and DevOps Guru.
+                            I worked with Senior Product Managers, Principal Engineers, Senior UX Designers,
+                            Documentation writers and Managers to launch new features for both {" "}
+                            <a href="https://aws.amazon.com/codeguru/">CodeGuru Profiler</a> and {" "}
+                            <a href="https://aws.amazon.com/devops-guru/">DevOps Guru</a>. I led projects such as DevOps
+                            Guru integration into RDS which led to a 30% increase in customers. I also designed,
+                            implemented, and launched new customer-facing features for both products, such as profile
+                            comparison visualizations and a new onboarding workflow.<br /><br />
+
+                            I also regularly improved and simplified development and operational processes such as: <br />
+                            - Improving our teams pipelines to be fully CD and ran a project to move all of the product's
+                            pipelines to full CD. <br />
+                            - Moved away from manually creating alarms and moved all our remaining infrastructure to CDK. <br />
+                            - Fixed over-provisioned databases which led to a ~$1 million saving annually.
+                        </p>
+                        <ul className="skills">
+                            <li>Java</li>
+                            <li>Typescript</li>
+                            <li>CDK</li>
+                            <li>JavaScript</li>
+                            <li>AWS</li>
+                            <li>React</li>
+                            <li>DynamoDB</li>
+                            <li>Lambda</li>
+                            <li>RDS</li>
+                        </ul>
                     </Col>
                 </Row>
-        
+
                 {/* Amazon SDE 2*/}
                 <Row className="experience-card">
                     <Col md={2}>
@@ -78,9 +78,9 @@ function Experience() {
                         <p>
                             Designed and implemented a predictive alarming system monitoring all Prime Video
                             metrics to detect anomalies across all 8000+ Prime Video devices. I worked at all
-                            levels of this project from requirement gathering to deployment and maintenance.<br/><br/>
-                            Lead portions of the project working with senior and junior engineers. Developed on 
-                            AWS cloud using technologies such as Lambda, S3, Kinesis, Sagemaker, SNS, SQS, Elasticsearch 
+                            levels of this project from requirement gathering to deployment and maintenance.<br /><br />
+                            Lead portions of the project working with senior and junior engineers. Developed on
+                            AWS cloud using technologies such as Lambda, S3, Kinesis, Sagemaker, SNS, SQS, Elasticsearch
                             and DynamoDB. Enabled team to perform full CD deployments.
                         </p>
                         <ul className="skills">
@@ -98,7 +98,7 @@ function Experience() {
                         </ul>
                     </Col>
                 </Row>
-    
+
                 {/* Amazon SDE 1*/}
                 <Row className="experience-card">
                     <Col md={2}>
@@ -111,7 +111,7 @@ function Experience() {
                             Developed tools to improve the speed and quality of software development
                             across Prime Video client organizations by building systems that close the
                             gap between the specific development and testing needs of Prime Video and
-                            the general-purpose tools provided by Amazon.<br/><br/>
+                            the general-purpose tools provided by Amazon.<br /><br />
                             Implemented and maintained an ELK stack (Elasticsearch, Logstash and Kibana)
                             which ingested all Prime Video logs (46TB a day) to help developers deep
                             dive into issues in their system. Designed, implemented and maintained a ticket
@@ -140,7 +140,7 @@ function Experience() {
                         </ul>
                     </Col>
                 </Row>
-    
+
                 <Row className="experience-card">
                     <Col md={2}>
                         <p>Sep 2016 - Apr 2017</p>
@@ -159,7 +159,7 @@ function Experience() {
                         </ul>
                     </Col>
                 </Row>
-        
+
                 <Row className="experience-card">
                     <Col md={2}>
                         <p>Jun 2016 - Aug 2016</p>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import {Col, Container, Row} from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
-    return (
-        <Container className="footer">
-          <Row className="socials">
-            <Col md={12}>   
-              <a className='link' href='https://www.linkedin.com/in/greg-thomson-3a2227129/'><FontAwesomeIcon className='grow' icon={faLinkedinIn} size='2x'/></a>
-              <a className='link' href='https://github.com/Greg12Thomson'><FontAwesomeIcon className='grow' icon={faGithub} size='2x' /></a>
-            </Col>
-          </Row>
-          <Row className="footnote">
-            <Col md={12}>     
-              <p>
-                © Greg Thomson 2023
-              </p>         
-            </Col>
-          </Row>
-        </Container>
-    );
+  return (
+    <Container className="footer">
+      <Row className="socials">
+        <Col md={12}>
+          <a className='link' href='https://www.linkedin.com/in/greg-thomson-3a2227129/'><FontAwesomeIcon className='grow' icon={faLinkedinIn} size='2x' /></a>
+          <a className='link' href='https://github.com/Greg12Thomson'><FontAwesomeIcon className='grow' icon={faGithub} size='2x' /></a>
+        </Col>
+      </Row>
+      <Row className="footnote">
+        <Col md={12}>
+          <p>
+            © Greg Thomson 2023
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer;
